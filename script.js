44657d90ee5b1c6db66050b6739a3a14e38e1560
Library-Management-System <kunaldas532001@outlook.com>
2025-10-16 11:22:36 +0530

    new

--- script.js
+++ script.js
@@ -11,6 +11,7 @@
 
 const API_BASE_URL = 'https://script.google.com/macros/s/AKfycbxHOYtvKHeb5z0bJm7w84bal-169HYBgjrkrSSeUFrkA5c3UF7-pTql8tXj2h_KL54wlg/exec';
 
+
 // ============================================================================
 // STATE MANAGEMENT
 // ============================================================================
